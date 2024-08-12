4ec26bbfe4ea60015d00c77fe7e422c97d41478e
kedroboroni <svishoyrik@mail.ru>
2024-08-12 17:02:57 +0300

    Создали стартовое меню плюс добавли кому-то описание

--- main.py
+++ main.py
@@ -3,6 +3,18 @@ from telebot import types
 
 bot = telebot.TeleBot("7242578162:AAGe1G8znafwQQA_3n4awLjzOKMtX1MXKkI")
 
+
+@bot.message_handler(commands = ["start"])
+def keyBoard(message):
+    markup = types.ReplyKeyboardMarkup() #Создали меняю разметки
+    btn1 = types.KeyboardButton("перейти на сайт") #Создали кнопку
+    btn2 = types.KeyboardButton("Удалить сообщение") #Создали кнопку
+    btn3 = types.KeyboardButton("Изменить сообщение") #Создали кнопку
+    markup.add(btn1) #Разместили
+    markup.row(btn2, btn3) #Разместили
+    bot.send_message(message.chat.id, "Привет!", reply_markup = markup)
+
+
 @bot.message_handler(commands = ["start", "main", "hello"])
 def main(message):
     print(type(message))
@@ -12,17 +24,17 @@ def main(message):
 
 @bot.message_handler(content_types = ["photo"])
 def processingPhoto(message):
-    
-    markup = types.InlineKeyboardMarkup()
-    markup.add(types.InlineKeyboardButton("перейти на сайт", url = "http://127.0.0.1:8000/"))
-    btn1 = types.InlineKeyboardButton("Удалить сообщение", callback_data = "delete")
-    btn2 = types.InlineKeyboardButton("Изменить сообщение", callback_data = "edit")
-    markup.row(btn1,btn2)
+    markup = types.InlineKeyboardMarkup() #Создали разметку
+    btn1 = types.InlineKeyboardButton("перейти на сайт", url = "http://127.0.0.1:8000/") #Создали кнопку
+    btn2 = types.InlineKeyboardButton("Удалить сообщение", callback_data = "delete") #Создали кнопку
+    btn3 = types.InlineKeyboardButton("Изменить сообщение", callback_data = "edit") #Создали кнопку
+    markup.add(btn1) #Разместили
+    markup.row(btn2, btn3) #Разместили
     bot.send_message(message.chat.id, "Вау, ты отправил мне фотографию", reply_markup = markup)
 
 @bot.callback_query_handler(func = lambda callback: True)
 def callback_message(callback):
-    if callback.data == "delete":
+    if callback.data == "delete": 
         bot.delete_message(callback.message.chat.id, callback.message.message_id - 1)
     elif callback.data == "edit":
         bot.edit_message_text("Мы изменили свое сообщение, и ты нам за это нчиего не сделаешь!!!", callback.message.chat.id, callback.message.message_id )
