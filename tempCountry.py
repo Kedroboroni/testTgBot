85cf1eb60797698b54262eda8e0ab2bd0b3fa1f5
kedroboroni <svishoyrik@mail.ru>
2024-08-12 22:51:20 +0300

    изменили токен бота, на другой.

--- tempCountry.py
+++ tempCountry.py
@@ -1,7 +1,7 @@
 import telebot
 from telebot import types
 
-bot = telebot.TeleBot("7242578162:AAGe1G8znafwQQA_3n4awLjzOKMtX1MXKkI")
+bot = telebot.TeleBot("7404560580:AAHAbYNTfQ95DFbu2wQM0EFvw2PDRN0GUi4")
 
 
 @bot.message_handler(comands = ["start"])
@@ -14,4 +14,4 @@ def get_country(message):
     country = message.text.strip().lower()
 
 
-bot.infinity_polling(none_stop = True)
+bot.polling(none_stop = True)
