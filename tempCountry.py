508a278a644107394b888e40282b902cd5da9db3
kedroboroni <svishoyrik@mail.ru>
2024-08-13 16:56:26 +0300

    Теперь работаем в ветке ДБ

--- tempCountry.py
+++ tempCountry.py
@@ -1,10 +1,10 @@
 import telebot
 from telebot import types
 
-bot = telebot.TeleBot("7404560580:AAHAbYNTfQ95DFbu2wQM0EFvw2PDRN0GUi4")
+bot = telebot.TeleBot("7242578162:AAGe1G8znafwQQA_3n4awLjzOKMtX1MXKkI")
+API = "41bc5d5f636a36078cb732f482d9ce0a"
 
-
-@bot.message_handler(comands = ["start"])
+@bot.message_handler(commands = ["start"])
 def start(message):
     bot.send_message(message.chat.id, "Привет, напиши свой город")
 
